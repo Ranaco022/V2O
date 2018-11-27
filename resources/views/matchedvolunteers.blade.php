5295d560a0abb5624976d9c9c07b90abe09546eb
Ranaco022 <ranaco13@yahoo.com>
2018-11-27 03:04:34 -0500

    Changes to volunteer display (card list)

--- resources/views/matchedvolunteers.blade.php
+++ resources/views/matchedvolunteers.blade.php
@@ -1,19 +1,32 @@
 @extends('layouts.app')
 
+@section('style')
+    <style>
+        .card-body p:hover
+        {
+            border-color: #00cc99;
+            cursor: pointer;
+        }
+    </style>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+@endsection
+
+
+
 @section('content')
 <div class="container-fluid">
-    <div class="row ">
-        <div class="col-lg-6">
+    <!--div class="row ">
+        <div-- class="col-lg-6">
             <div class="card">
-                <div class="card-header">Volunteers Matched to {{Auth::user()->name }}</div>
+                <div class="card-header">Volunteers Matched to {{Auth::user()->name }}</div-->
 
-                <div class="card-body">
+                <!--div class="card-body"-->
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                    <!--a href="{{ url('volorg') }}">Back to Main Page</a-->
+                    <!--a href="{{ url('volorg') }}">Back to Main Page</a>
                     <h2>List of volunteers interested in your cause</h2>
                     <br>
 
@@ -38,7 +51,32 @@
                     <td class="tg-buh4">{{ $matched_volunteer->emailAddress }}</td>
                   </tr>
                 @endforeach
-                </table>
+                </table-->
+                        <div class="container-fluid">
+                            @if($matched_volunteers != null)
+                                <div class="row">
+                                     @foreach ($matched_volunteers as $matched_volunteer)
+                                        <div class="col-sm-3 col-md-4">
+                                            <div class="card">
+                                                <div class="card-body">
+                                                    <p>Name: {{ $matched_volunteer->firstName }} {{ $matched_volunteer->lastName }}</p>
+                                                    <p>Email: {{ $matched_volunteer->emailAddress }}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                     @endforeach
+                                </div>
+                            @else
+                                <h1>There are currently no interested volunteers</h1>
+                            @endif
+                        </div>
+                                <!--td class="tg-buh4">{{ $matched_volunteer->firstName }}</td>
+                                <td class="tg-buh4">{{ $matched_volunteer->lastName }}</td>
+                                <td class="tg-buh4">{{ $matched_volunteer->homeAddress }}</td>
+                                <td class="tg-buh4">{{ $matched_volunteer->cellNumber }}</td>
+                                <td class="tg-buh4">{{ $matched_volunteer->homeNumber }}</td>
+                                <td class="tg-buh4">{{ $matched_volunteer->emailAddress }}</td-->
+
 
                 <a href="{{ url('volorg') }}">Back to Main Page</a>
 
@@ -46,7 +84,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-6">
+
+
+
+        <!--div class="col-lg-6">
             <div class="card">
                 <div class="card-header">Current Volunteers of {{Auth::user()->name }}</div>
 
@@ -56,7 +97,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <!--a href="{{ url('volorg') }}">Back to Main Page</a-->
+                    <a href="{{ url('volorg') }}">Back to Main Page</a>
                         <h2>List of current volunteers in your organization</h2>
                         <br>
 
@@ -73,7 +114,7 @@
                         </div>
                 </div>
             </div>
-        </div>
+        </div-->
     </div>
 </div>
 @endsection
