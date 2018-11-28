b9eae575100d83a3e7298b99314eae699cf565f6
namastayinschool <namastayinschool@github.com>
2018-11-28 00:53:43 -0400

    Progress to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,33 +6,37 @@
           <img src="{{ asset('/img/banner.png') }}" style="width:100%">
 
 <div style="margin:auto">
-          <table style="height: 354px;" width="735";  >
-	<tbody>
-		<tr>
-			<td style="width: 183.5px;">&nbsp;<img src="/img/dog-2.svg" style="width: 50%; height:50%; text-align: center;"></td>
-			<td style="width: 183.5px;">&nbsp;<img src="/img/teddy-bear.svg" style="width: 50%; height:50%"></td>
-			<td style="width: 183.5px;">&nbsp;<img src="/img/couple.svg" style="width: 50%; height:50%"></td>
-			<td style="width: 183.5px;">&nbsp;<img src="/img/football.svg" style="width: 50%; height:50%"></td>
-		</tr>
-		<tr>
-			<td style="width: 183.5px; text-align: center;">&nbsp;
-				<h4>Animal Care</h4>
-				<p>This rewarding volunteer role would suit applicants that love animals and are committed to improving their welfare.</p>
-			</td>
-			<td style="width: 183.5px; text-align: center;">&nbsp;
-				<h4>Child Care</h4>
-				<p>People who volunteer with kids get to provide education, healthcare, and most importantly loving attention to these young souls.</p>
-			</td>
-			<td style="width: 183.5px; text-align: center;">&nbsp;
-				<h4>Senior Citizens Care</h4>
-				<p>Whether it&rsquo;s at a community center or a nursing home, there are many opportunities to spend time with seniors all across the country.</p>
-			</td>
-			<td style="width: 183.5px; text-align: center;">&nbsp;
-				<h4>Sports</h4>
-				<p>Lead after school clubs and leagues, physical education lessons, and community sports. Help young people have fun.</p>
-			</td>
-		</tr>
-	</tbody>
+  <table style="height: 354px; margin: auto;" width="735" cellpadding="4">
+<tbody>
+<tr style="height: 145.59375px;">
+<td style="width: 183.5px; text-align: center; height: 165.59375px;">&nbsp;<img style="width: 70%; height: 70%;" src="/img/dog-2.svg" alt="" /></td>
+<td style="width: 183.5px; text-align: center; height: 165.59375px;">&nbsp;<img style="width: 70%; height: 70%;" src="/img/teddy-bear.svg" alt="" /></td>
+<td style="width: 183.5px; text-align: center; height: 165.59375px;">&nbsp;<img style="width: 70%; height: 70%;" src="/img/couple.svg" alt="" /></td>
+<td style="width: 183.5px; text-align: center; height: 165.59375px;">&nbsp;<img style="width: 70%; height: 70%;" src="/img/football.svg" alt="" /></td>
+</tr>
+<tr style="height: 118px;">
+<td style="width: 183.5px; height: 118px; text-align: center; vertical-align: top;">&nbsp;
+<h4>Animal Care</h4>
+<p>&nbsp;</p>
+<p>This rewarding volunteer role would suit applicants that love animals and are committed to improving their welfare.</p>
+</td>
+<td style="width: 183.5px; height: 118px; text-align: center; vertical-align: top;">&nbsp;
+<h4>Child Care</h4>
+<p>&nbsp;</p>
+<p>People who volunteer with kids get to provide education, healthcare, and most importantly loving attention to these young souls.</p>
+</td>
+<td style="width: 183.5px; height: 118px; text-align: center; vertical-align: top;">&nbsp;
+<h4>Senior Citizens Care</h4>
+&nbsp;
+<p>Whether it&rsquo;s at a community center or a nursing home, there are many opportunities to spend time with seniors all across the country.</p>
+</td>
+<td style="width: 183.5px; height: 118px; text-align: center; vertical-align: top;">&nbsp;
+<h4>Sports</h4>
+<p>&nbsp;</p>
+<p>Lead after school clubs and leagues, physical education lessons, and community sports. Help young people have fun.</p>
+</td>
+</tr>
+</tbody>
 </table>
 </div>
 <!-- DivTable.com -->
