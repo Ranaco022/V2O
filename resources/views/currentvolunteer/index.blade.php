360818b72a7c5cb6c557fb6605bdb5f9702c84c6
namastayinschool <namastayinschool@github.com>
2018-11-30 09:25:50 -0400

    Styled Volunteer Organisation dashboard

--- resources/views/currentvolunteer/index.blade.php
+++ resources/views/currentvolunteer/index.blade.php
@@ -1,54 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-<div class="content">
-    <div>
-        <div class="title m-b-md">
-            <h2>List of Current Volunteers</h2>
-        </div>
-        <div class="col-md-4">
-            <a class="btn btn-info btn-md" href="{{ url('currentvolunteer/create') }}">ADD RECORDS</a>
-        </div>
+<style>
+  .uper {
+    margin-top: 40px;
 
 
-        <div class = "col-sm-12 col-md-9">
+  }
+</style>
+<div class="uper">
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}
+    </div><br />
+  @endif
+  <br>
+  <h3> List of current volunteers </h3>
 
-            <table class="table table-striped">
-                <tr>
-                    <th class="tg-ddb2">First Name</th>
-                    <th class="tg-0lax">Last Name</th>
-                    <th class="tg-0lax">Address</th>
-                    <th class="tg-0lax">Email</th>
-                    <th class="tg-0lax">Home #</th>
-                    <th class="tg-0lax">Cell #</th>
-                    <th class="tg-0lax">Hours Worked</th>
+<a class="btn btn-info" href="{{ url('currentvolunteer/create') }}">Add Record</a>
 
-                </tr>
-                @if(!empty($currentvolunteers))
-                    @foreach ($currentvolunteers as $currentvolunteer)
-                            <tr>
-                                <td class="tg-buh4">{{$currentvolunteer->firstName}}</td>
-                                <td class="tg-buh4">{{$currentvolunteer->lastName}}</td>
-                                <td class="tg-buh4">{{$currentvolunteer->homeAddress}}</td>
-                                <td class="tg-buh4">{{$currentvolunteer->emailAddress}}</td>
-                                <td class="tg-buh4">{{$currentvolunteer->homeNumber}}</td>
-                                <td class="tg-buh4">{{$currentvolunteer->cellNumber}}</td>
-                                <td class="tg-buh4">{{$currentvolunteer->hoursWorked}}</td>
-                                <th><a class="btn btn-info btn-md" href="/currentvolunteer/{{$currentvolunteer->id}}/edit" class="">Edit</a></th>
-
-                                <th>
-                                    {!! Form::open(array('method' => 'DELETE', 'url' =>'/timetable/' .$currentvolunteer->id, 'class' => '')) !!}
-                                         {!!Form::submit('Delete', array('class' => 'class="btn btn-danger btn-md "' )) !!}
-                                    {!!Form::close() !!}
-                                </th>
-                            </tr>
-                    @endforeach
-                @endif
-            </table>
-        </div>
-    </div>
-</div>
-</div>
-
+<br>
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>First Name</td>
+          <td>Last Name</td>
+          <td>Address</td>
+          <td>E-mail</td>
+          <td>Home</td>
+          <td>Cell</td>
+          <td>Hours Worked</td>
+          <td colspan="2">Action</td>
+        </tr>
+    </thead>
+    <tbody>
+      @if(!empty($currentvolunteers))
+        @foreach($currentvolunteers as $currentvolunteer)
+        <br>
+        <tr>
+            <td>{{$currentvolunteer->firstName}}</td>
+            <td>{{$currentvolunteer->lastName}}</td>
+            <td>{{$currentvolunteer->homeAddress}}</td>
+            <td>{{$currentvolunteer->emailAddress}}</td>
+            <td>{{$currentvolunteer->homeNumber}}</td>
+            <td>{{$currentvolunteer->cellNumber}}</td>
+            <td>{{$currentvolunteer->hoursWorked}}</td>
+            <td><a href="{{ route('currentvolunteer.edit',$currentvolunteer->id)}}" class="btn btn-primary">Edit</a></td>
+            <td>
+                <form action="{{ route('currentvolunteer.destroy', $currentvolunteer->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+      @endif
+    </tbody>
+  </table>
 @endsection
